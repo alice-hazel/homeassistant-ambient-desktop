2b2f3adc123bd8ae152f2a1dfc51e1d515ee3398
Alice Gardner <git@alicehazel.dev>
2026-08-30 17:22:03 +1000

    Add type hinting and clean some code

--- ambient.py
+++ ambient.py
@@ -1,14 +1,25 @@
 import cv2
 import numpy as np
 from math import sqrt, floor
-import numpy.typing
+import typing
+import cv2.typing as cv2t
+import numpy.typing as npt
+
+type Colour = np.ndarray[tuple[int], np.dtype[np.uint8]]
+type Colours = np.ndarray[tuple[int, int], np.dtype[np.uint8]]
 
 
-def from_file(path):
-    return cv2.imread(path)
+def from_file(path: str) -> typing.Union[cv2t.MatLike | None]:
+    """
+    Read a file as an image
+    """
+    return cv2.imread(path, cv2.IMREAD_COLOR_BGR)
 
 
-def preview(image, preview_height=400):
+def preview(image: cv2.typing.MatLike, preview_height: int=400) -> None:
+    """
+    Display the given image using imshow
+    """
     height, width, _ = image.shape
     image = cv2.resize(image, (round(width * (preview_height / height)), preview_height), interpolation=cv2.INTER_NEAREST)
 
@@ -16,7 +27,10 @@ def preview(image, preview_height=400):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def preview_colours(image, colours, preview_size=20):
+def preview_colours(image: cv2.typing.MatLike, colours: Colours, preview_size: int = 20) -> None:
+    """
+    Draw a palette of colours over an image and display it on screen
+    """
     height, width, _ = image.shape
     size = min(width, height) // preview_size
 
@@ -26,21 +40,23 @@ def preview_colours(image, colours, preview_size=20):
     preview(image)
 
 
-def quantize_image(image, samples):
-    arr = np.array(image)
+def quantize_image(image: cv2.typing.MatLike, samples: int) -> Colours:
+    """
+    """
+    arr = np.array(image, dtype=np.uint8)
     height, width, _ = arr.shape
     pixels = arr.reshape(height*width, 3)
     # slim selected colours down by getting rid of boring pixels
     pixels = build_bgr_palette(pixels)
 
     if len(pixels) < 1:
-        return []
+        return np.array([])
 
     iters = floor(sqrt(samples))
     return split_colourspace(pixels, iters)
     
 
-def split_colourspace(colours, depth):
+def split_colourspace(colours: Colours, depth: int) -> Colours:
     # Calculate the max-min of each colour channel
     channel_ranges = [
         np.max(colours[:, 0]) - np.min(colours[:,0]),
@@ -60,34 +76,38 @@ def split_colourspace(colours, depth):
 
     next_depth = depth - 1
     if next_depth == 0:
-        return mean_colour(part1), mean_colour(part2)
+        return np.vstack((mean_colour(part1), mean_colour(part2)))
 
     return np.concatenate((split_colourspace(part1, next_depth), split_colourspace(part2, next_depth)))
 
-def mean_colour(pixels):
-    return np.mean(pixels, axis=0)
+def mean_colour(colours: Colours) -> Colour:
+    return np.mean(colours, axis=0).astype(np.uint8)
 
-def convert(*colours, flag):
+def convert(*colours: Colours, flag: cv2.ColorConversionCodes) -> Colours:
+    """
+    Convert colours between two colourspaces according to the given flag
+    """
     if len(colours) < 1:
-        return []
+        return np.array([])
 
     # Treat colours as a set of pixels in an image
     # Read ints as 8bit and then reshape (NColours, 3) to (1, NColours, 3)
-    colours = np.array(colours, dtype=np.uint8)[np.newaxis, :, :3]
-    colours = cv2.cvtColor(colours, flag)
-    # Get the list of colours back
-    colours = np.squeeze(colours)
+    colour_array = np.array(colours, dtype=np.uint8)[np.newaxis, :, :3]
+    colour_img = cv2.cvtColor(colour_array, flag)
 
-    return colours
+    # Get the list of colours back
+    new_colour_array = np.array(colour_img, dtype=np.uint8)
+    new_colours = np.squeeze(new_colour_array, axis=0)
+
+    return new_colours
 
 
 # Note: hsv in opencv is from 0-255, not as a %
-def build_bgr_palette(colours, min_value=50, min_saturation=80):
+def build_bgr_palette(colours: Colours, min_value: int = 50, min_saturation: int = 80) -> Colours:
     # OpenCV will open images as BGR because gr
     # For most operations we dont care, but here we do, so reinterpret the colours
     if len(colours) < 1:
-        return []
-
+        return np.array([])
 
     colours = convert(*colours, flag=cv2.COLOR_BGR2HSV)
     
@@ -109,7 +129,7 @@ def build_bgr_palette(colours, min_value=50, min_saturation=80):
     colours = colours[value_order]
 
     if len(colours) < 1:
-        return []
+        return np.array([])
     
     # Convert back to BGR
     return convert(*colours, flag=cv2.COLOR_HSV2BGR)
@@ -120,7 +140,11 @@ if __name__ == "__main__":
 
     # img = from_camera()
     img = from_file("samples/1.png")
+    if not img:
+        exit()
+
     colours = quantize_image(img, 16)
     colours = build_bgr_palette(colours)
 
-    print(colours)
+    # print(colours)
+    preview_colours(img, colours)
--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import cv2
+import cv2.typing as cv2t
 import ambient
 import requests
 from time import sleep
 from dotenv import load_dotenv
 from os import getenv
+import numpy as np
 
 SAMPLES = 16
 UPDATE_DELAY = 1
 CAM_INDEX = 0
 
-load_dotenv()
-HASS_TOKEN = getenv("HASS_TOKEN")
-HASS_API = getenv("HASS_ENDPOINT")
-ENTITY = getenv("HASS_ENTITY")
-
-
-def from_camera(index=0):
+def from_camera(index=0) -> cv2t.MatLike:
     dev = cv2.VideoCapture(index)
 
     if not dev.isOpened():
@@ -28,10 +24,13 @@ def from_camera(index=0):
 
     return frame
 
-def push_colour(colour):
-    r, g, b = colour
+def push_colour(colour: ambient.Colour) -> None:
+    r, g, b = colour.tolist()
 
-    url = f"{HASS_API}/states/{ENTITY}"
+    url = f"{HASS_ENDPOINT}/states/{HASS_ENTITY}"
+    print(url)
+    print(f"RGB {r} {g} {b}")
+    return
     result = requests.post(
         url, 
         headers={"Authorization": f"Bearer {HASS_TOKEN}"},
@@ -48,26 +47,37 @@ def push_colour(colour):
     result.raise_for_status()
 
 
-def screen_to_ha(index):
+def screen_to_ha(index: int) -> None:
     img = from_camera(index)
     
     colours = ambient.quantize_image(img, SAMPLES)
     colours = ambient.build_bgr_palette(colours)
 
     if len(colours) < 1:
+        print("No colours found")
         return
 
-    hsv = ambient.convert(colours[0], flag=cv2.COLOR_BGR2HSV)
-    # boost ~~saturation~~ and value for prettiness
-    # hsv = (hsv[0], hsv[1], 255)
-    hsv = (hsv[0], 255, 255)
-    colour = ambient.convert(hsv, flag=cv2.COLOR_HSV2RGB)
+    hsv_colours = ambient.convert(colours[0], flag=cv2.COLOR_BGR2HSV)
+    hsv_colour = hsv_colours[0]
 
-    colour = [int(c) for c in colour]
+    hsv_colour = np.array(
+        # boost saturation and value for prettiness
+        (hsv_colour.item(0), 255, 255), 
+        dtype=np.uint8
+    )
+
+    colours = ambient.convert(hsv_colour, flag=cv2.COLOR_HSV2RGB)
+    # There is only one colour, but .convert returns a list of colours
+    colour = colours[0]
 
     push_colour(colour)
 
 if __name__ == "__main__":
+    load_dotenv()
+    HASS_TOKEN = getenv("HASS_TOKEN")
+    HASS_ENTITY = getenv("HASS_ENTITY")
+    HASS_ENDPOINT = getenv("HASS_ENDPOINT")
+
     while True:
         screen_to_ha(CAM_INDEX)
         sleep(UPDATE_DELAY)
